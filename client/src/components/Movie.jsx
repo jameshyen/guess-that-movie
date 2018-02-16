94cb2c9143c94912cf5192ca9c47d5cdd79e2ea4
James Yen <jameshyen@gmail.com>
2018-02-16 13:08:08 -0800

    Remove unnecessary

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 const Movie = ({ movie }) => {
-  const style = {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  }
   return (
     <div>
-      <img style={style} src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
-      <h4 style={style}>{movie.title}</h4>
+      <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
+      <h4>{movie.title}</h4>
     </div>
   );
 }
