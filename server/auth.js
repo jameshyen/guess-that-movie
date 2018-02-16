97e269149a6682618112a11b960651a8879d666d
James Yen <jameshyen@gmail.com>
2018-02-16 15:16:38 -0800

    Le refactor

---
+++ server/auth.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const path = require('path');
+const session = require('express-session');
+
+const db = require('../database');
+
+module.exports.login = {
+  GET: function (req, res) {
+    res.status(200).sendFile(path.join(__dirname, '/../client/auth/login.html'));
+  },
+  POST: function (req, res) {
+    db.User.findOne({ username: req.body.username }, function (err, user) {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, match) {
+          if (match) {
+            req.session.regenerate(function () {
+              req.session.user = req.body.username;
+              res.status(201).redirect('/');
+            });
+          } else {
+            res.status(400).end('Incorrect combination of username and password!');
+          }
+        })
+      } else {
+        res.status(400).end('No such user found in our database!'); // lol
+      }
+    });
+  },
+};
+
+module.exports.signup = {
+  GET: function (req, res) {
+    res.status(200).sendFile(path.join(__dirname, '/../client/auth/signup.html'));
+  },
+  POST: function (req, res) {
+    db.User.find({ username: req.body.username }, function (err, user) {
+      if (user.length !== 0) {
+        res.status(400).end('Username already taken!');
+      } else {
+        bcrypt.hash(req.body.password, 10, function (err, hash) {
+          (new db.User({
+            username: req.body.username,
+            password: hash,
+          })).save().then(function (user) {
+            req.session.regenerate(function () {
+              req.session.user = user.username;
+              res.status(201).redirect('/');
+            });
+          });
+        });
+      }
+    });
+  },
+};
+
+module.exports.logout = {
+  POST: function (req, res) {
+    req.session.destroy(function () {
+      res.status(200).redirect('/login');
+    });
+  },
+}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const bcrypt = require('bcrypt');
-const path = require('path');
 
-const { User, Movie } = require('../database'); // Database models...
-const { API_KEY } = require('../config'); // API key for TheMovieDB...
+const db = require('../database');
+const api_key = require('../config');
+const auth = require('./auth');
 
 const app = express();
 
@@ -18,52 +17,11 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/login', function (req, res) {
-  res.status(200).sendFile(path.join(__dirname, '/../client/auth/login.html'));
-});
-
-app.post('/login', function (req, res) {
-  User.findOne({ username: req.body.username }, function (err, user) {
-    if (user) {
-      bcrypt.compare(req.body.password, user.password, function (err, match) {
-        if (match) {
-          req.session.regenerate(function () {
-            req.session.user = req.body.username;
-            res.status(201).redirect('/');
-          });
-        } else {
-          res.status(400).end('Incorrect combination of username and password!');
-        }
-      })
-    } else {
-      res.status(400).end('No such user found in our database!'); // lol
-    }
-  });
-});
-
-app.get('/signup', function (req, res) {
-  res.status(200).sendFile(path.join(__dirname, '/../client/auth/signup.html'));
-});
-
-app.post('/signup', function (req, res) {
-  User.find({ username: req.body.username }, function (err, user) {
-    if (user.length !== 0) {
-      res.status(400).end('Username already taken!');
-    } else {
-      bcrypt.hash(req.body.password, 10, function (err, hash) {
-        (new User({
-          username: req.body.username,
-          password: hash,
-        })).save().then(function (user) {
-          req.session.regenerate(function () {
-            req.session.user = user.username;
-            res.status(201).redirect('/');
-          });
-        });
-      });
-    }
-  });
-});
+app.get('/login', auth.login.GET);
+app.post('/login', auth.login.POST);
+app.get('/signup', auth.signup.GET);
+app.post('/signup', auth.signup.POST);
+app.post('/logout', auth.logout.POST);
 
 app.use(function (req, res, next) {
   if (req.session && req.session.user) {
@@ -77,13 +35,13 @@ app.get('/movie', function (req, res) {
   let movieId = 0;
   axios.get('https://api.themoviedb.org/3/movie/latest', {
     params: {
-      api_key: API_KEY,
+      api_key: api_key,
     }
   }).then(function ({ data }) {
     movieId = Math.floor(Math.random() * (data.id + 1));
     return axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
       params: {
-        api_key: API_KEY,
+        api_key: api_key,
         language: 'en-US',
       }
     });
