f71b6befd9653ff52b403431285d483c36839b9f
James Yen <jameshyen@gmail.com>
2018-02-16 12:05:00 -0800

    switch to ES6

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var axios = require('axios');
+var { API_KEY } = require('../config/themoviedb.js');
 
 var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.listen(3000, function() {
+app.get('/movie', function (req, res) {
+  var high = 0;
+  axios.get('https://api.themoviedb.org/3/movie/latest', {
+    params: {
+      api_key: API_KEY,
+    }
+  }).then(function ({ data }) {
+    
+  });
+});
+
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
