808375f0d9224bbdda3cbcd304e7f89d289a6fff
James Yen <jameshyen@gmail.com>
2018-02-16 18:17:30 -0800

    plain styling better

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -5,15 +5,12 @@ const Movie = ({ movie, guessed }) => {
     filter: 'blur(5px)',
   };
   return (
-    <figure className='figure'>
-      <img className='figure-img img-fluid rounded' src={`http://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}></img>
+    <div>
+      <img src={`http://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}></img>
       <h4 style={style}>{movie.title}</h4>
-      <figcaption className='figure-caption text-center'>{movie.overview}</figcaption>
-    </figure>
+      <p>{movie.overview}</p>
+    </div>
   );
 }
 
 export default Movie;
-
-/*       <h4 style={style}>{movie.title}</h4>
-      <p>{movie.overview}</p> */
