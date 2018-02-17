1e95e432de8a46a668846d338a8c8a752ff32da8
James Yen <jameshyen@gmail.com>
2018-02-16 18:15:54 -0800

    Meh bootstrap

--- client/src/components/Guess.jsx
+++ client/src/components/Guess.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Guess = ({ guess, attempt, next, score }) => {
   return (
     <div>
-      <input onChange={attempt} />&nbsp;
-      <button onClick={guess}>Submit</button>&nbsp;
-      <button onClick={next}>Next</button>&nbsp;
+      <input className='form-control' onChange={attempt} />&nbsp;
+      <button type='button' className='btn btn-outline-primary btn-sm' onClick={guess}>Submit</button>&nbsp;
+      <button type='button' className='btn btn-outline-primary btn-sm' onClick={next}>Next</button>&nbsp;
       Score: {score}
     </div>
   );
--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -5,12 +5,15 @@ const Movie = ({ movie, guessed }) => {
     filter: 'blur(5px)',
   };
   return (
-    <div>
-      <img src={`http://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}></img>
+    <figure className='figure'>
+      <img className='figure-img img-fluid rounded' src={`http://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}></img>
       <h4 style={style}>{movie.title}</h4>
-      <p>{movie.overview}</p>
-    </div>
+      <figcaption className='figure-caption text-center'>{movie.overview}</figcaption>
+    </figure>
   );
 }
 
 export default Movie;
+
+/*       <h4 style={style}>{movie.title}</h4>
+      <p>{movie.overview}</p> */
